bcb3cad86d13aeda94f30d315fa63f058daa50d0
Lan Le Tu <lan.letu96@gmail.com>
2018-12-01 02:19:51 -0500

    USer variables

--- DonationTracker/DonationTracker/Models/User.swift
+++ DonationTracker/DonationTracker/Models/User.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+class User {
+    private var userID: String?
+    private var userName: String
+    private var password: String
+    private var failedAttemps: Int
+    private var lastFailed: Int?
+    private var type: UserType?
+    private var isLock: Bool?
+    private var contact: Contact?
+    private var assignedLocation: String
+    
+    init(userName: String, password: String, failedAttemps: Int, type: UserType, assignedLocation: String) {
+        self.userName = userName
+        self.assignedLocation = assignedLocation
+        self.password = password
+        self.failedAttemps = failedAttemps
+    }
+}
